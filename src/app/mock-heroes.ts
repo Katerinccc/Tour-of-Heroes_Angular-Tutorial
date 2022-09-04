efb34a1a8e1e81594f42ac94b5db99f4ffd786d0
Katerin Calderon <kccc0713@gmail.com>
2022-09-04 11:11:45 -0500

    separate the heroes component of the hero detail in a new component. Add heroes mock data, add services of heroes and messages

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 12, name: 'Webstorm' },
+  { id: 13, name: 'Pegasus' },
+  { id: 14, name: 'Venus' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'Dinoman' },
+  { id: 17, name: 'Dynamite' },
+  { id: 18, name: 'Flux' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
